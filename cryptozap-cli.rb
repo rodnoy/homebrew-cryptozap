872524f1ddb2bd6f2031de5060160daafcaf320c
Kirill Simagin <ksimagin@gmail.com>
2025-04-23 23:40:01 +0200

    link updates

--- cryptozap-cli.rb
+++ cryptozap-cli.rb
@@ -1,7 +1,7 @@
 class CryptozapCli < Formula
   desc "Secure file encryption and decryption tool"
   homepage "https://github.com/rodnoy/CryptoZap"
-  url "https://github.com/rodnoy/CryptoZap/releases/download/1.0.2/cryptozap-cli-app.tar.gz"
+  url "https://github.com/rodnoy/CryptoZap/releases/download/v1.0.2/cryptozap-cli-app.tar.gz"
   sha256 "db4520c395e0ffddfbf941a2fbdad9fa21039a570dd5a0327a6b454cce52235c"
   license "MIT"
 
