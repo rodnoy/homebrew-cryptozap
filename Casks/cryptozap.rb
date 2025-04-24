fd9a25f6b9ab3841dd80da1aeb636d1f60b00fe2
Kirill Simagin <ksimagin@gmail.com>
2025-04-24 10:22:03 +0200

    fighting with cli

--- Casks/cryptozap.rb
+++ Casks/cryptozap.rb
@@ -2,7 +2,7 @@ cask "cryptozap" do
   version "1.0.2"
   sha256 "485ece994bf5cf383798c672a67a408e2d78e8de568884a07b2264ff1ae0da1f"
 
-  url "https://github.com/rodnoy/CryptoZap/releases/download/v1.0.2/CryptoZap.1.0.dmg"
+  url "https://github.com/rodnoy/CryptoZap/releases/download/v1.0.2.1/CryptoZap.1.0.dmg"
   name "CryptoZap"
   desc "Graphical app for secure file encryption and decryption"
   homepage "https://github.com/rodnoy/CryptoZap"
--- cryptozap-cli.rb
+++ cryptozap-cli.rb
@@ -1,13 +1,14 @@
 class CryptozapCli < Formula
   desc "Secure file encryption and decryption tool"
   homepage "https://github.com/rodnoy/CryptoZap"
-  url "https://github.com/rodnoy/CryptoZap/releases/download/v1.0.2/cryptozap-cli-app.tar.gz"
-  sha256 "39b1f865983dde6579e5fbef22a88751dcf805e21e84630aed517c55a9d59b14"
+  url "https://github.com/rodnoy/CryptoZap/releases/download/v1.0.2.1/cryptozap-cli-app-v1.0.3.tar.gz"
+  sha256 "855e99f2942a4833d1bfee693400e0b714db64abff29be49769389f7f963239d"
   license "MIT"
 
   def install
     bin.install "cryptozap-cli"
     man1.install "cryptozap-cli.1.tar.gz" => "cryptozap-cli.1"
+    prefix.install "CryptoEngine_CryptoEngine.bundle"
   end
 
   test do
