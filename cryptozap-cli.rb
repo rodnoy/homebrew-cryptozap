48af70b6d250aa2d50827064d7631137ac00b637
Kirill Simagin <ksimagin@gmail.com>
2025-04-24 11:16:12 +0200

    v1.0.2.1 fixed decrypt

--- cryptozap-cli.rb
+++ cryptozap-cli.rb
@@ -2,7 +2,7 @@ class CryptozapCli < Formula
   desc "Secure file encryption and decryption tool"
   homepage "https://github.com/rodnoy/CryptoZap"
   url "https://github.com/rodnoy/CryptoZap/releases/download/v1.0.2.1/cryptozap-cli-app-v1.0.2.1.tar.gz"
-  sha256 "2c2df29eadaf967011b28309ad8e5bd5c58d37982a24e40ed858fcbefbacfcff"
+  sha256 "91eac5ab23053a148a20b426dd0905ee8ab1af3827496f01ce999e6994aec6c9"
   license "MIT"
 
   def install
