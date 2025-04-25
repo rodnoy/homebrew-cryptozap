8befc55e52f076fe44abed314b5b0f142b26ae6b
Kirill Simagin <ksimagin@gmail.com>
2025-04-25 10:08:05 +0200

    update hash for universal binary

--- cryptozap-cli.rb
+++ cryptozap-cli.rb
@@ -2,7 +2,7 @@ class CryptozapCli < Formula
   desc "Secure file encryption and decryption tool"
   homepage "https://github.com/rodnoy/CryptoZap"
   url "https://github.com/rodnoy/CryptoZap/releases/download/v1.0.2.1/cryptozap-cli-app-v1.0.2.1.tar.gz"
-  sha256 "91eac5ab23053a148a20b426dd0905ee8ab1af3827496f01ce999e6994aec6c9"
+  sha256 "26fb789221e2b57d156b4a4ac911c4656339d938a5bbf58216260092bb295eab"
   license "MIT"
 
   def install
